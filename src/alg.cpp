89d18a01507626d06f07a8ceaf26cb5d463c1e60
lidiiakuznetsova <76099410+lidiiakuznetsova@users.noreply.github.com>
2021-05-27 23:05:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,85 +2,90 @@
 #include <string>
 #include "tstack.h"
 
-int stackqueue (char sq){
-switch (sq){
-default: return -1;
-case '(': return 0; break;
-case ')': return 1; break;
-case '+': return 2; break;
-case '-': return 2; break;
-case '/': return 3; break;
-case '*': return 3; break;
-}}
-int operations (char oper, int before, int after){
-switch (oper) {
-default: return -1;   
-case '+': return before + after; break;
-case '-': return before - after; break;
-case '*': return before * after; break;
-case '/': return before / after; break;	
-}}
+int p(char count) {
+    switch (count) {
+      case '(':
+          return 0;
+      case ')':
+          return 1;
+      case '+':
+      case '-':
+          return 2;
+      case '*':
+      case '/':
+          return 3;
+      default:
+          return -1;
+    }
+}
 std::string infx2pstfx(std::string inf) {
-TStack <char> stack1;
-std::string str;
-char current = 0, symbol;
-int queue;
-for (int i = 0; i < inf.length(); i++) {
-symbol = inf[i];
-queue = stackqueue(symbol);
-if (queue > -1) {
-if ((queue == 0 || queue > stackqueue(current) || stack1.isEmpty()) && symbol != ')'){
-if (stack1.isEmpty()){
-current = symbol;
-stack1.push(symbol)
+    TStack<char> stack;
+    std::string r;
+
+    for (int i = 0; i < inf.length(); i++) {
+        if ((inf[i] >= '0') && (inf[i] <= '9')) {
+            r += inf[i];
+            r += ' ';
+        } else if (inf[i] == '(') {
+            stack.push(inf[i]);
+        } else if (p(inf[i]) > p(stack.get()) || stack.isEmpty()) {
+            stack.push(inf[i]);
+        } else if (inf[i] == ')') {
+            while (!stack.isEmpty() && stack.get() != '(') {
+                r += stack.get();
+                r += ' ';
+                stack.pop();
+            }
+
+            if (stack.get() == '(') {
+               stack.pop();
+            }
+        } else {
+            while (!stack.isEmpty() && p(stack.get()) >= p(inf[i])) {
+                r += stack.get();
+                r += ' ';
+                stack.pop();
+            }
+
+            stack.push(inf[i]);
+        }
+    }
+
+    while (!stack.isEmpty()) {
+        r += stack.get();
+        r += ' ';
+        stack.pop();
+    }
+
+    while (r[r.length() - 1] == ' ') {
+        r = r.substr(0, r.length()-1);
+    }
+
+    return r;
 }
-else if (symbol == ')') {
-while (stack1.get() != '(') {
-str.push_back(stack1.get());
-str.push_back(' ');
-stack1.pop();
-}
-if (stack1.isEmpty())
-current = 0;
-}
-else {
-while (!stack1.isEmpty() && stackqueue(stack1.get()) >= queue)){
-str.push_back(stack1.get());
-str.push_back(' ');
-stack1.pop();
-if (stack1.isEmpty()){
-current = inf[i];
-stack1.push(inf[i]);
-}}}
-}
-else {
-str.push_back(symbol);
-str.push_back(' ');
-}}
-while (!stack1.isEmpty()) {
-str.push_back(stack1.get());
-str.push_back(' ');
-stack1.pop();
-}
-str.erase(str.end() - 1, str.end());
-return str;  
-}
-int eval(std::string pst) {
-TStack<int> stack2;
-std::string str2;
-int before, after, result;
-for (int i = 0; i < pst.length(); i++) {
-if (pst[i] >= '0' && pst[i] <= '9') {
-str2 = pst[i];
-stack2.push(pst[i] - '0');
-} 
-else if (pst[i] != ' ') {
-after = stack2.get();
-stack2.pop();
-before = stack2.get();
-stack2.pop();
-if (pst[i] == '*'||pst[i] =='-'||pst[i] =='/'||pst[i] =='+'){
-result = operations(pst[i], before, after);
-}}
-return stack2.get();
+
+    TStack<int> stack;
+
+    for (int i = 0; i < pst.length(); i++) {
+        if ((pst[i] >= '0') && (pst[i] <= '9')) {
+            stack.push(pst[i] - '0');
+        } else if (pst[i] != ' ') {
+            int second = stack.get();
+            stack.pop();
+            int first = stack.get();
+            stack.pop();
+
+            if (pst[i] == '-') {
+                stack.push(first - second);
+            } else if (pst[i] == '+') {
+                stack.push(first + second);
+            } else if (pst[i] == '*') {
+                stack.push(first * second);
+            } else {
+                stack.push(first / second);
+            }
+        }
+    }
+
+    return stack.get();
 }
